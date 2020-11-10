65c562b1f72a6d49ce81285473b40e741a67f68d
Long Huynh <Longhuynh741@yahoo.com>
2020-11-10 02:17:46 -0500

    created models folder. added code to get total duration working and exporting workout

---
+++ models/Workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        weight: {
+            type: Number,
+            required: false,
+        },
+        sets: {
+            type: Number,
+            required: false,
+        },
+        reps: {
+            type: Number,
+            required: false,
+        },
+        duration: {
+            type: Number,
+            required: false,
+        },
+        distance: {
+            type: Number,
+            required: false,
+        }
+    }]
+}, { toJSON: { virtuals: true } });
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
